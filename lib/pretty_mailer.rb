00d48e5aa84ebac3c24baf3855aed285e2918fd3
Adam Ross Cohen <a.ross.cohen@gmail.com>
2013-06-02 04:28:46 -0400

    this works ok...

--- lib/pretty_mailer.rb
+++ lib/pretty_mailer.rb
@@ -1,5 +1,54 @@
 require "pretty_mailer/version"
+require 'css_parser'
+require 'nokogiri'
 
 module PrettyMailer
-  # Your code goes here...
+  
+  class << self
+    
+    def included base
+      base.class_eval do
+        alias_method_chain :collect_responses_and_parts_order, :inline_styles
+      end
+    end
+    
+  end
+  
+  def collect_responses_and_parts_order_with_inline_styles headers
+    responses, parts_order = collect_responses_and_parts_order_without_inline_styles headers
+    responses.map! do |response|
+      if response[:content_type] == 'text/html'
+        apply_inline_styles response, headers[:css]
+      else
+        response
+      end
+    end
+    [responses, parts_order]
+  end
+  
+  def apply_inline_styles response, stylesheets
+    doc = Nokogiri::HTML response[:body]
+    [*stylesheets].each do |stylesheet|
+      parsed_css( stylesheet ).each_selector do |selector, declarations, specificity|
+        doc.css( selector ).each do |tag|
+          if tag['style'].blank?
+            tag['style'] = "#{ declarations }"
+          else
+            declarations.split( ';' ).each do |rule|
+              tag['style'] = tag['style'].sub /#{rule.split(':').first.strip}:\s*[a-z0-9]+\s*;/i, rule
+            end
+          end
+        end
+      end
+    end
+    response[:body] = doc.to_s
+    response
+  end
+  
+  def parsed_css stylesheet
+    parser = CssParser::Parser.new
+    parser.load_string! Rails.application.assets.find_asset( stylesheet ).to_s
+    parser
+  end
+  
 end
